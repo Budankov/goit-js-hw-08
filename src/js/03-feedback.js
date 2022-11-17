e0ed140ac51b0450c4cc2c06ae6f9ad54ecd8685
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-17 19:48:35 +0200

    Update 02-video.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateTextarea();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage['message'] || '';
+    refs.input.value = savedMessage['email'] || '';
+  }
+}
