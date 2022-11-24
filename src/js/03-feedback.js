af660e4ec8342753754eac6fc1c2c82a67898314
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-24 19:45:28 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,40 @@
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
-const formEmail = document.querySelector('.feedback-form [name="email"]');
-const formTextArea = document.querySelector('[name="message"]');
-const formSubmit = document.querySelector('.feedback-form');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
 
 const STORAGE_FORM = 'feedback-form-state';
 
-const formData = {};
+refs.form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+window.addEventListener('load', updateOutputOnload);
 
-onPageReload();
-
-formSubmit.addEventListener('input', throttle(onFormInput), 500);
-formSubmit.addEventListener('submit', onFormSubmit);
-
-function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_FORM, JSON.stringify(formData));
+function onInputForm(e) {
+  e.preventDefault();
+  const message = refs.form.elements.message.value;
+  const email = refs.form.elements.email.value;
+  localStorage.setItem(STORAGE_FORM, JSON.stringify({ message, email }));
 }
 
-function onPageReload() {
-  const savedData = localStorage.getItem(STORAGE_FORM);
-  const parsedData = JSON.parse(savedData);
-
-  if (parsedData) {
-    formEmail.value = parsedData.email;
-  }
-  if (parsedData) {
-    formTextArea.value = parsedData.message;
-  }
+function updateOutputOnload(e) {
+  e.preventDefault();
+  const outputTextContent = localStorage.getItem(STORAGE_FORM);
+  const outputObjectContent = JSON.parse(outputTextContent) || {
+    email: '',
+    message: '',
+  };
+  const { email, message } = outputObjectContent;
+  refs.form.elements.email.value = email;
+  refs.form.elements.message.value = message;
 }
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  formData.email = formEmail.value;
-  formData.message = formTextArea.value;
-
-  console.log('FINAL FORM DATA ', formData);
-  formSubmit.reset();
-  localStorage.removeItem(STORAGE_FORM);
+function onSubmitForm(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  localStorage.clear();
+  refs.form.reset();
 }
